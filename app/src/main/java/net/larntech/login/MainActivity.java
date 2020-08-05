268f5be1f0b08cbed4af31c7068fe51158a314e1
Richard Kamere <rkamere@gmail.com>
2020-08-05 13:55:10 +0300

    initial commit

---
+++ app/src/main/java/net/larntech/login/MainActivity.java
@@ -0,0 +1,25 @@
+package net.larntech.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"No Action",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
